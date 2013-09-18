ca9809d0d9dbd1d93ddd9a6cd5e6009e5013c8eb
Jeff Uphoff <juphoff@eucalyptus.com>
2013-09-18 15:03:39 -0300

    Stub standard output/error capturing capability for OS X; fixes #5.

--- daemon_darwin.go
+++ daemon_darwin.go
@@ -3,10 +3,30 @@ package godaemon
 // Copyright (c) 2013 VividCortex, Inc. All rights reserved.
 // Please see the LICENSE file for applicable license terms.
 
-// Daemonize is a no-op on MacOSX Darwin.
-func Daemonize(child bool) {
+import (
+	"io"
+	"os"
+)
+
+// A DaemonAttr describes the options that apply to daemonization
+type DaemonAttr struct {
+	CaptureOutput bool // whether to capture stdout/stderr
 }
 
-// DaemonizeWithCapture is a no-op on MacOSX Darwin.
-func DaemonizeWithCapture(child bool) (io.Reader, io.Reader) {
+// MakeDaemon is a no-op on MacOSX Darwin.
+func MakeDaemon(attrs *DaemonAttr) (io.Reader, io.Reader) {
+	var stdout, stderr *os.File
+
+	if attrs.CaptureOutput {
+		stdout = os.NewFile(uintptr(1), "stdout")
+		stderr = os.NewFile(uintptr(2), "stderr")
+	}
+	return stdout, stderr
+}
+
+// Daemonize is equivalent to MakeDaemon(&DaemonAttr{}). It is kept only for
+// backwards API compatibility, but its usage is otherwise discouraged. Use
+//
+// Daemonize is a no-op on MacOSX Darwin.
+func Daemonize(child ...bool) {
 }
