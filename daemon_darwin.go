08817a493f3281e262b7fd25f6d4f8fb0d04872d
marpaia <marpaia@etsy.com>
2013-09-18 23:08:52 -0400

    add the MakeDaemon function to daemon_darwin.go so that running MakeDaemon doesn't error out when running on OS X

--- daemon_darwin.go
+++ daemon_darwin.go
@@ -7,11 +7,21 @@ import (
 // Copyright (c) 2013 VividCortex, Inc. All rights reserved.
 // Please see the LICENSE file for applicable license terms.
 
+// A DaemonAttr describes the options that apply to daemonization
+type DaemonAttr struct {
+	CaptureOutput bool // whether to capture stdout/stderr
+}
+
 // Daemonize is a no-op on MacOSX Darwin.
 func Daemonize(child bool) {
 }
 
 // DaemonizeWithCapture is a no-op on MacOSX Darwin.
 func DaemonizeWithCapture(child bool) (io.Reader, io.Reader) {
+	return nil, nil
+}
+
+// MakeDaemon is a no-op on MacOSX Darwin.
+func MakeDaemon(attrs *DaemonAttr) (io.Reader, io.Reader) {
 	return nil, nil
 }
